b348e66c1c17c041bdc7ca23e0096c54fb2c31df
hafidnurkholis <itsmaehere@gmail.com>
2023-07-10 13:18:41 +0700

    fix: fixing data error

--- src/pages/careers/Careers.jsx
+++ src/pages/careers/Careers.jsx
@@ -2,27 +2,26 @@ import { Link, useLoaderData } from "react-router-dom"
 
 export default function Careers() {
 
-    const careers = useLoaderData();
-    console.log(careers)
+  const data = useLoaderData();
 
-    return (
-        <>
-         <div className="careers">
-            {careers.map(career => (
-        <Link to={career.id.toString()} key={career.id}>
+  return (
+    <>
+      <div className="careers">
+        {data.careers.map(career => (
+          <Link to={career.id.toString()} key={career.id}>
             <p>{career.title}</p>
             <p>Based in {career.location}</p>
-        </Link>
-      ))}
-    </div>
+          </Link>
+        ))}
+      </div>
     </>
   )
 }
 
 // Data Loader
 export const careersLoader = async () => {
-    const res = await fetch('https://yulidarmaulana.github.io/netninja-react-router/data/db.json')
-    // const res = await fetch('https://649f60ee245f077f3e9d85dc.mockapi.io/api/v1/')
+  const res = await fetch('https://yulidarmaulana.github.io/netninja-react-router/data/db.json')
+  // const res = await fetch('https://649f60ee245f077f3e9d85dc.mockapi.io/api/v1/')
 
-    return res.json()
+  return res.json()
 }   
