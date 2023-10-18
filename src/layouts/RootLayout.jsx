3bbddeb527c5c39a127594e39992c5534b089018
yulidarmaulana <31232250+yulidarmaulana@users.noreply.github.com>
2023-10-18 07:44:21 +0700

    feat : add tailwindcss, layouting home, and adjust routing

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -2,13 +2,13 @@ import { Outlet, NavLink } from "react-router-dom";
 export default function RootLayout() {
   return (
     <div className="root-layout">
-      <header>
+      <header className="mt-2">
         <nav>
-          <h1>Jobarouter</h1>
+          <h1 className="text-2xl font-bold">Another Landing</h1>
           <NavLink to="/">Home</NavLink>
           <NavLink to="about">About</NavLink>
-          <NavLink to="help">Help</NavLink>
-          <NavLink to="careers">Careers</NavLink>
+          {/* <NavLink to="help">Help</NavLink> */}
+          <NavLink to="careers">Coming Soon</NavLink>
         </nav>
       </header>
       <main>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,30 @@
 export default function Home() {
   return (
     <div className="home">
-      <h2>Welcome</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum dolor
-        assumenda, itaque nesciunt alias voluptatum nobis blanditiis eos
-        officiis aperiam earum eum vel quas odio optio, distinctio ab sunt unde
-        incidunt ipsum omnis amet magnam accusantium aut! Excepturi, cupiditate
-        iusto!
+      <p className="text-center font-medium">
+        Welcome to Another Landing Page <br /> Another landing is a collection
+        of designs and slicing desings that I have made so far.
       </p>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus
-        veniam ducimus eligendi nihil, cumque ab eveniet modi architecto quidem,
-        non odit saepe facere voluptas esse mollitia illo fuga exercitationem id
-        dicta iusto eaque numquam quaerat ad! Fugit velit beatae laudantium.
-      </p>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus
-        veniam ducimus eligendi nihil, cumque ab eveniet modi architecto quidem,
-        non odit saepe facere voluptas esse mollitia illo fuga exercitationem id
-        dicta iusto eaque numquam quaerat ad! Fugit velit beatae laudantium.
-      </p>
+
+
+      <div className="">
+        <div className="max-w-md rounded overflow-hidden shadow-lg bg-slate-800">
+          <img
+            className="w-full h-48 p-4"
+            src="/public/vite.svg"
+            alt=""
+          />
+          <div className="px-6 py-4">
+            <div className="font-bold text-gray-200 text-xl">something</div>
+            <p className="text-gray-200 font-light">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Voluptatibus quia, nulla! Maiores et perferendis eaque,
+              exercitationem praesentium nihil.
+            </p>
+            <button type="button" className="inline-block mb-2 px-6 py-2.5 bg-violet-400 text-white hover:bg-blue-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 font-medium text-xs w-full rounded-lg">Visit</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
